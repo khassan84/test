0f8b775ef884157ac367d74312ab94efd793201c
khassan84 <kamalhassan.bme@gmail.com>
2019-11-03 00:34:52 +0530

    test commit

---
+++ test/src/test/Test.java
@@ -0,0 +1,12 @@
+package test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String text = "[{options=[{text=o1}, {text=o2}, {text=o3}, {text=o4}], answerType=radio, answer=[o1], choice=o1, text=q1}, {answer=[o3], options=[{text=o1}, {text=o2}, {text=o3}, {text=o4}], text=q2, answerType=radio, choice=o3}]";
+
+	}
+
+}
