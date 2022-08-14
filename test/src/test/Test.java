f0af64e2f62edcbb20e02b29dedbe3610174bedd
Kamal Hassan <57215467+khassan84@users.noreply.github.com>
2022-08-14 16:23:28 +0530

    Update Test.java
    test

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -7,6 +7,8 @@ public class Test {
 		
 		String text = "[{options=[{text=o1}, {text=o2}, {text=o3}, {text=o4}], answerType=radio, answer=[o1], choice=o1, text=q1}, {answer=[o3], options=[{text=o1}, {text=o2}, {text=o3}, {text=o4}], text=q2, answerType=radio, choice=o3}]";
 
+	
+	System.out.println("test");
 	}
 
 }
